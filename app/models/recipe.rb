733a5b1483b510ac147509a517e1421ec2604732
Jack <dowsanjack@gmail.com>
2015-01-12 11:44:40 +0530

    initial

---
+++ app/models/recipe.rb
@@ -0,0 +1,5 @@
+class Recipe < ActiveRecord::Base
+
+	DIFFICULTY = %w(Easy Medium Hard)
+	
+end
---
+++ db/migrate/20150111191721_create_food_preferences.rb
@@ -0,0 +1,9 @@
+class CreateFoodPreferences < ActiveRecord::Migration
+  def change
+    create_table :food_preferences do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
